c460178b80d0925e1166c21fbcc42f2f7aa75d78
Benjamin Sitbon <benjamin.sitbon@ratpdev.com>
2023-10-02 15:18:00 +0200

    Nouvelle branche

--- code.py
+++ code.py
@@ -1,3 +1,7 @@
-from datetime import datetime
+# from datetime import datetime
 
-print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+# print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+
+from module import obtenir_temps
+
+print(obtenir_temps())
